7d8f611a74873ba70cc683f35288d63a7fe51509
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-03 23:53:08 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,4 +1,4 @@
-require_relative '.current_age_for_birth_year.rb'
+require_relative './current_age_for_birth_year.rb'
 puts "What year were you born?"
 birth_year = gets.to_i
 users_age = current_age_for_birth_year(birth_year) + (Time.now.year - 2003)
